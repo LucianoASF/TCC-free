91c7c12eea6b2969bce1ad3b65e5ba3e6f9f866e
LucianoASF <lucianoaugustos12@gmail.com>
2025-03-11 18:09:17 -0300

    Colocando url do backend

--- frontend/src/axios.config.js
+++ frontend/src/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'tcc-free-production.up.railway.app';
 
 // Interceptor para adicionar o token dinamicamente
 axios.interceptors.request.use((config) => {
